692bac29c683982b87bc80ffc549444c1e3dab86
unknown <artem.ryabinin.1990@gmail.com>
2020-04-23 15:12:57 +0300

    Настройка авторизации пользователя. Перенос html кода в файлики html

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,6 @@ $(document).on('click', '.navbar-toggle-btn', function () {
     $('.navbar-wrap-conteiner').toggleClass('navbar-toggle');
     $('.AppContent-wrapper').toggleClass('AppContent-toggle');
 });
+function logout(link) {
+    document.location.href =link;
+}
