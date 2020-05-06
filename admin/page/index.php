4a9bd8b608914bab5fde4e68f2f5ba46c07fb7c2
unknown <artem.ryabinin.1990@gmail.com>
2020-05-06 16:16:18 +0300

    Новые страница для администрирования на сайте

---
+++ admin/page/index.php
@@ -0,0 +1,37 @@
+<?php
+
+include './../../config.php';
+
+include './../../auth.php';
+
+$page = "";
+$titlePage = $titlePage . " | Администрирование - страниц сайта";
+
+//header
+$pageH = file_get_contents($pageHeader);
+$page = $pageH;
+
+//menu
+$pageM = file_get_contents($pageMenu);
+$page = $page . $pageM;
+
+//content
+$pageC = file_get_contents("./page.html");
+$page = $page . $pageC;
+
+//fouter
+$contentJsScript = '<script src="' . $host . '/admin/page/content.js" type="text/javascript"></script>';
+$pageF = file_get_contents($pageFouter);
+
+//Замена переменных в шаблоне
+$page = $page . $pageF;
+$page = str_replace("%\$titlePage%", $titlePage, $page);
+$page = str_replace("%\$host%", $host, $page);
+$page = str_replace("%\$userName%", $_SESSION["Name"], $page);
+$page = str_replace("%\$userFamily%", $_SESSION["Family"], $page);
+$page = str_replace("%\$contentJsScript%", $contentJsScript, $page);
+$page = str_replace("%\$contentCss%", $contentCss, $page);
+
+
+echo $page;
+?>
