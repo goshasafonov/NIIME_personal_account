fd59a615cd30325160a8da80a67de93878fa03d3
unknown <artem.ryabinin.1990@gmail.com>
2020-05-08 19:12:50 +0300

    Добавлены всплывающие сообщения

--- admin/page/content.js
+++ admin/page/content.js
@@ -1,7 +1,39 @@
-/*global selectMdiIcon */
+/*global exampleLargeMdiIcon, formCreatePage_MdiIcon */
 
-selectMdiIcon.innerHTML = "";
+//selectMdiIcon.innerHTML = "";
 
 $(document).ready(function () {
-    fillMdiIconInSelectPicker(selectMdiIcon);
+    fillMdiIconInSelectPicker(formCreatePage_MdiIcon);
 });
+
+function createNewPage() {
+    var formData = new FormData(formCreatePage);
+    formData.append('queryId', 'getRoute');
+     $.ajax({
+        url: 'ajax.php',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            try {
+                var jsonData = JSON.parse(data);
+                if (typeof jsonData.AjaxError !== "undefined") {
+                    //alertMsg(jsonData.AjaxError);
+                    createToast("Error", jsonData.AjaxError);    
+                } else {
+                    alert(1);
+                }
+            } catch (e) {
+                createToast("Error", "Ошибка в структуре ответа от сайта:<br>" + e);                 
+            }
+        },
+        error: function (request, status, error) {
+            createToast("Error", "Ошибка при обращении к серверу:<br>error:" + error + "<br>status:" + status);
+        }
+    });
+}
+
+function showExampleMdiIcon(sel) {
+    exampleLargeMdiIcon.setAttribute("class","text-muted text-center "+sel.value);
+}
